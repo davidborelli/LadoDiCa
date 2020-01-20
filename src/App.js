6b5bf4f08e9a847b20748403391c3b9c7deee55e
David <david_borelli@live.com>
2020-01-20 16:31:06 -0300

    Configuração Redux, Redux-Saga, Reactotron-redux/saga

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from '~/styles/global';
 import '~/config/ReactotronConfig';
+
+import store from './store';
 
 import Header from '~/components/Header';
 import Category from '~/components/Category';
@@ -11,11 +15,13 @@ import Routes from './routes';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Category />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Category />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
