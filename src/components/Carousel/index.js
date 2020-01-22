54f889eb659b1ecd0b64e9f8ebd62807b90e71a9
David <david_borelli@live.com>
2020-01-22 01:53:51 -0300

    Substituição do Carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import Slider from 'infinite-react-carousel';
+import Carousel from 'react-multi-carousel';
 
 import { Link } from 'react-router-dom';
 import api from '~/services/api';
+import * as S from './styles';
 
-export default function Carousel() {
+export default function CarouselList() {
   const [highlights, setHighlights] = useState([]);
 
   useEffect(() => {
@@ -12,32 +13,54 @@ export default function Carousel() {
       const response = await api.get('/highlights');
 
       setHighlights(response.data);
-      console.tron.log(response.data);
     };
 
     loadhighlights();
   }, []);
 
-  const settings = {
-    autoplay: true,
-    dots: true,
-    arrows: false,
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
   };
 
   return (
-    <Slider {...settings} style={{ width: '100%' }}>
-      {highlights.map(highlight => (
-        <div key={highlight.id}>
-          <Link
-            to={{
-              pathname: `/detail/${highlight.id}`,
-              state: { product: highlight },
-            }}
-          >
-            <img src={highlight.pics[0].path} alt={highlight.pics[0].name} />
-          </Link>
-        </div>
-      ))}
-    </Slider>
+    <S.Container>
+      <Carousel
+        responsive={responsive}
+        showDots
+        autoPlay
+        autoPlaySpeed={3000}
+        centerMode
+        infinite
+        removeArrowOnDeviceType={['tablet', 'mobile', 'desktop']}
+      >
+        {highlights.map(highlight => (
+          <div key={highlight.id}>
+            <Link
+              to={{
+                pathname: `/detail/${highlight.id}`,
+                state: { product: highlight },
+              }}
+            >
+              <img src={highlight.pics[0].path} alt={highlight.pics[0].name} />
+            </Link>
+          </div>
+        ))}
+      </Carousel>
+    </S.Container>
   );
 }
--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -1,13 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 900px;
-  height: 200px;
-  margin: 10px auto;
-
-  display: flex;
-  justify-content: stretch;
-  align-items: center;
-
-  background: #999;
+  img {
+    width: 200px;
+    height: 200px;
+  }
 `;
